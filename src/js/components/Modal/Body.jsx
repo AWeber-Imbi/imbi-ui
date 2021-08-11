3832605946f68752253a8e68f84c7908f7a9d289
Gavin M. Roy <gavinr@aweber.com>
2021-08-11 13:47:10 -0400

    Use HeadlessUI for Modal

--- src/js/components/Modal/Body.jsx
+++ src/js/components/Modal/Body.jsx
@@ -1,13 +1,21 @@
+import { Dialog } from '@headlessui/react'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 class Body extends React.PureComponent {
   render() {
     return (
-      <div className={`${this.props.className}`}>{this.props.children}</div>
+      <Dialog.Description className={`${this.props.className}`}>
+        {this.props.children}
+      </Dialog.Description>
     )
   }
 }
+
+Body.defaultProps = {
+  className: 'text-gray-500'
+}
+
 Body.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.array,
--- src/js/components/Modal/Footer.jsx
+++ src/js/components/Modal/Footer.jsx
@@ -1,12 +1,20 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { Button } from '../'
+
 class Footer extends React.PureComponent {
   render() {
     return (
       <div
-        className={`mt-5 sm:mt-6 text-right border-t border-t-gray-400 pt-5 mt-5 space-x-3 ${this.props.className}`}>
+        className={`mt-5 sm:mt-6 text-right border-t border-gray-400 pt-5 mt-5 space-x-3 ${this.props.className}`}>
         {this.props.children}
+        {this.props.onClose !== undefined &&
+          this.props.closeText !== undefined && (
+            <Button key="modal-close" onClick={this.props.onClose}>
+              {this.props.closeText}
+            </Button>
+          )}
       </div>
     )
   }
@@ -16,6 +24,8 @@ Footer.propTypes = {
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element
   ]).isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
+  closeText: PropTypes.string,
+  onClose: PropTypes.func
 }
 export { Footer }
