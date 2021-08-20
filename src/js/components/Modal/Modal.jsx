b410e7281bcee1d15545fa22604aa181c8b9427e
Gavin M. Roy <gavinr@aweber.com>
2021-08-20 14:24:59 -0400

    Fix focus behavior

--- src/js/components/Modal/Modal.jsx
+++ src/js/components/Modal/Modal.jsx
@@ -9,19 +9,22 @@ import { Footer } from './Footer'
 class Modal extends React.PureComponent {
   constructor(props) {
     super(props)
+    this.dialogRef = React.createRef()
     this.state = { isOpen: true }
   }
 
   render() {
     return (
       <Dialog
+        initialFocus={this.dialogRef}
         open={this.state.isOpen}
         onClose={() => this.setState({ isOpen: false })}
         className="fixed font-sans z-10 inset-0 overflow-y-auto text-base">
         <div className="flex items-center justify-center min-h-screen">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
           <div
-            className={`align-bottom sm:align-middle bg-white cursor-default focus:outline-none focus:ring-0 inline-block sm:max-w-2xl sm:my-8 overflow-hidden px-4 py-5 rounded-lg sm:p-6 shadow-xl text-left transform transition-all sm:w-full ${this.props.className}`}>
+            className={`align-bottom sm:align-middle bg-white cursor-default focus:outline-none focus:ring-0 inline-block sm:max-w-2xl sm:my-8 overflow-hidden px-4 py-5 rounded-lg sm:p-6 shadow-xl text-left transform transition-all sm:w-full ${this.props.className}`}
+            ref={this.dialogRef}>
             {this.props.children}
           </div>
         </div>
@@ -39,6 +42,7 @@ Modal.propTypes = {
     PropTypes.element
   ]).isRequired,
   className: PropTypes.string,
+  focus: PropTypes.object,
   onClose: PropTypes.func
 }
 export { Modal }
