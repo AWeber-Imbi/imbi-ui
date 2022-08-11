3d6516999db7f72f5457d9323d8ca1d43c8a1234
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-08-11 13:48:11 -0400

    Remove unnecessary sortDirection state
    Since sort direction is defined in the column property, there is
    no reason to separately maintain its value in the component state.
    The column.sortCallback will trigger the property change and redraw
    when necessary.

--- src/js/components/Table/HeadColumn.jsx
+++ src/js/components/Table/HeadColumn.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { Column } from '../../schema'
 import { Icon } from '../'
@@ -17,25 +17,19 @@ const ColClassName =
   'align-middle px-6 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider whitespace-nowrap'
 
 function HeadColumn({ column, children, className, disabled, srOnly }) {
-  const [sortDirection, setSortDirection] = useState(column.sortDirection)
-  useEffect(() => {
-    if (
-      column.sortCallback !== undefined &&
-      sortDirection !== column.sortDirection
-    ) {
-      column.sortCallback(column.name, sortDirection)
-    }
-  }, [sortDirection])
-
   function onSortClick(event) {
     event.preventDefault()
     if (disabled) return
-    if (sortDirection === null) {
-      setSortDirection(Asc)
-    } else if (sortDirection === Asc) {
-      setSortDirection(Desc)
-    } else if (sortDirection === Desc) {
-      setSortDirection(null)
+
+    if (column.sortCallback !== undefined) {
+      const direction =
+        column.sortDirection === null
+          ? Asc
+          : column.sortDirection === Asc
+          ? Desc
+          : null
+
+      column.sortCallback(column.name, direction)
     }
   }
 
@@ -68,8 +62,10 @@ function HeadColumn({ column, children, className, disabled, srOnly }) {
       onClick={onSortClick}>
       {column.sortCallback !== undefined && (
         <Icon
-          icon={SortIcon[sortDirection]}
-          className={`mr-2 ${sortDirection !== null ? 'text-blue-600' : ''}`}
+          icon={SortIcon[column.sortDirection]}
+          className={`mr-2 ${
+            column.sortDirection !== null ? 'text-blue-600' : ''
+          }`}
         />
       )}
       {children !== undefined && children}
