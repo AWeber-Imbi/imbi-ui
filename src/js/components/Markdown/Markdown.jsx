c86b4678de751735740ad1f21f6005a75cb9c3e3
Dave Shawley <daves@aweber.com>
2022-09-26 17:06:20 -0400

    Enable styling in Markdown field
    Tailwind disables heading styles so I added them explicitly to the
    Markdown component.  I also fixed a small type error in the Link
    component.

--- src/js/components/Markdown/Markdown.jsx
+++ src/js/components/Markdown/Markdown.jsx
@@ -19,18 +19,50 @@ Link.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.element,
     PropTypes.arrayOf(PropTypes.element),
+    PropTypes.arrayOf(PropTypes.string),
     PropTypes.string
   ]),
   href: PropTypes.string
 }
+
 class UL extends React.PureComponent {
   render() {
-    return (
-      <ul className="list-disc list-inside space-y-2">{this.props.children}</ul>
-    )
+    return <ul className="list-disc list-inside">{this.props.children}</ul>
   }
 }
 UL.propTypes = {
+  children: PropTypes.any
+}
+
+class Paragraph extends React.PureComponent {
+  render() {
+    return <p className="pb-1">{this.props.children}</p>
+  }
+}
+Paragraph.propTypes = {
+  children: PropTypes.any
+}
+
+class Heading extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.level = props.level
+  }
+  render() {
+    if (this.level === 1) {
+      return <h1 className="text-xl pb-2 text-bold">{this.props.children}</h1>
+    }
+    if (this.level === 2) {
+      return <h2 className="text-lg pb-1 text-bold">{this.props.children}</h2>
+    }
+    if (this.level === 3) {
+      return <h3 className="pb-1 text-bold">{this.props.children}</h3>
+    }
+    return <p className="text-bold">{this.props.children}</p>
+  }
+}
+Heading.propTypes = {
+  level: PropTypes.number.isRequired,
   children: PropTypes.any
 }
 
@@ -50,7 +82,11 @@ class Markdown extends React.PureComponent {
         className={this.props.className}
         components={{
           a: ({ ...props }) => <Link {...props} />,
-          ul: ({ ...props }) => <UL {...props} />
+          ul: ({ ...props }) => <UL {...props} />,
+          h1: ({ ...props }) => <Heading level={1} {...props} />,
+          h2: ({ ...props }) => <Heading level={2} {...props} />,
+          h3: ({ ...props }) => <Heading level={3} {...props} />,
+          p: ({ ...props }) => <Paragraph {...props} />
         }}>
         {this.props.children}
       </ReactMarkdown>
