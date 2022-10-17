4aa059b77dfd95f8e9b25fbb86c0b9b37f80311d
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-10-17 15:12:06 -0400

    Remove unnecessary Fragment wrapper

--- src/js/views/Project/Details.jsx
+++ src/js/views/Project/Details.jsx
@@ -35,67 +35,65 @@ function Display({ project, onEditClick, shouldGrow }) {
   const { t } = useTranslation()
   return (
     <Card className={`flex flex-col ${shouldGrow ? 'h-full' : ''}`}>
-      <Fragment>
-        <h2 className="font-medium mb-2">{t('terms.projectInfo')}</h2>
-        <dl className="lg:ml-4 my-3 space-y-3 overflow-hidden text-gray-900">
-          <Definition term={t('terms.namespace')} icon={project.namespace_icon}>
-            {project.namespace}
+      <h2 className="font-medium mb-2">{t('terms.projectInfo')}</h2>
+      <dl className="lg:ml-4 my-3 space-y-3 overflow-hidden text-gray-900">
+        <Definition term={t('terms.namespace')} icon={project.namespace_icon}>
+          {project.namespace}
+        </Definition>
+        <Definition term={t('terms.projectType')} icon={project.project_icon}>
+          {project.project_type}
+        </Definition>
+        <Definition term={t('terms.slug')} className="font-mono">
+          {project.slug}
+        </Definition>
+        {project.environments && project.environments.length > 0 && (
+          <Definition term={t('terms.environments')}>
+            {project.environments.join(', ')}
           </Definition>
-          <Definition term={t('terms.projectType')} icon={project.project_icon}>
-            {project.project_type}
-          </Definition>
-          <Definition term={t('terms.slug')} className="font-mono">
-            {project.slug}
-          </Definition>
-          {project.environments && project.environments.length > 0 && (
-            <Definition term={t('terms.environments')}>
-              {project.environments.join(', ')}
-            </Definition>
-          )}
-          {project.environments &&
-            project.environments.map((environment) => {
-              if (project.urls[environment] === undefined) return null
-              return (
-                <Definition
-                  key={`display-${environment}-url`}
-                  term={`${environment} URL`}>
-                  <a
-                    className="text-blue-800 hover:text-blue-700"
-                    title={project.urls[environment]}
-                    href={project.urls[environment]}
-                    target="_new">
-                    <Icon icon="fas external-link-alt" className="mr-2" />
-                    {project.urls[environment]}{' '}
-                  </a>
-                </Definition>
-              )
-            })}
-          {project.links.map((link, index) => {
+        )}
+        {project.environments &&
+          project.environments.map((environment) => {
+            if (project.urls[environment] === undefined) return null
             return (
-              <Definition key={`display-link-${index}`} term={link.title}>
+              <Definition
+                key={`display-${environment}-url`}
+                term={`${environment} URL`}>
                 <a
                   className="text-blue-800 hover:text-blue-700"
-                  href={link.url}
-                  title={link.url}
+                  title={project.urls[environment]}
+                  href={project.urls[environment]}
                   target="_new">
                   <Icon icon="fas external-link-alt" className="mr-2" />
-                  {link.url}{' '}
+                  {project.urls[environment]}{' '}
                 </a>
               </Definition>
             )
           })}
-        </dl>
-        {project.archived === false && (
-          <div className="flex-grow flex flex-row items-end">
-            <div className="flex-grow text-right mt-2">
-              <Button className="btn-white text-xs" onClick={onEditClick}>
-                <Icon icon="fas edit" className="mr-2" />
-                {t('project.editProject')}
-              </Button>
-            </div>
+        {project.links.map((link, index) => {
+          return (
+            <Definition key={`display-link-${index}`} term={link.title}>
+              <a
+                className="text-blue-800 hover:text-blue-700"
+                href={link.url}
+                title={link.url}
+                target="_new">
+                <Icon icon="fas external-link-alt" className="mr-2" />
+                {link.url}{' '}
+              </a>
+            </Definition>
+          )
+        })}
+      </dl>
+      {project.archived === false && (
+        <div className="flex-grow flex flex-row items-end">
+          <div className="flex-grow text-right mt-2">
+            <Button className="btn-white text-xs" onClick={onEditClick}>
+              <Icon icon="fas edit" className="mr-2" />
+              {t('project.editProject')}
+            </Button>
           </div>
-        )}
-      </Fragment>
+        </div>
+      )}
     </Card>
   )
 }
