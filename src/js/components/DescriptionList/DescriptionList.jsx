79ab386b514b7933cb3bfbfd1006493d16706572
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-10-26 12:00:43 -0400

    Narrow margins between DescriptionList items

--- src/js/components/DescriptionList/DescriptionList.jsx
+++ src/js/components/DescriptionList/DescriptionList.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 function DescriptionList({ children }) {
   return (
-    <dl className="lg:ml-4 my-3 space-y-6 overflow-hidden text-gray-900">
+    <dl className="lg:ml-4 my-3 space-y-3 overflow-hidden text-gray-900">
       {children}
     </dl>
   )
