23dbf9e4f70a6ce15fe19940267d095ac1c5d100
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-10-31 11:42:22 -0400

    Coalesce redundant Icon logic in Column

--- src/js/components/Table/Column.jsx
+++ src/js/components/Table/Column.jsx
@@ -9,7 +9,8 @@ function Column({ definition, children, linkTo }) {
   let clsName = ''
   let value = children
   if (definition !== undefined && definition.tableOptions !== undefined) {
-    if (definition.type === 'icon') value = ''
+    if (definition.type === 'icon')
+      value = <Icon className="mr-2" icon={children} title={children} />
     if (definition.tableOptions.className !== undefined)
       clsName = definition.tableOptions.className
     if (definition.tableOptions.lookupFunction !== undefined)
@@ -21,21 +22,11 @@ function Column({ definition, children, linkTo }) {
         <Link
           className={`align-middle h-full inline-block px-5 py-1.5 w-full whitespace-nowrap ${clsName}`}
           to={linkTo}>
-          {definition !== undefined && definition.type === 'icon' && (
-            <Icon className="mr-2" icon={children} title={children} />
-          )}
           {value}
         </Link>
       </td>
     )
-  return (
-    <td className={`px-5 py-1.5 whitespace-nowrap ${clsName}`}>
-      {definition !== undefined && definition.type === 'icon' && (
-        <Icon className="mr-2" icon={children} title={children} />
-      )}
-      {value}
-    </td>
-  )
+  return <td className={`px-5 py-1.5 whitespace-nowrap ${clsName}`}>{value}</td>
 }
 Column.propTypes = {
   definition: PropTypes.exact(ColumnPropTypes),
