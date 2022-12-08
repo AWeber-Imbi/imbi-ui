76812c40dfcce1394ffe0a48005b23006b5c9ccc
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-12-08 10:43:03 -0500

    Fix activity feed list React keys
    'key' props in list children need to be on the immediate React node
    descendants in the component tree, not the DOM tree. Which in this
    case is the Fragment that wraps the entry component and the divider,
    not its children.

--- src/js/views/Dashboard/ActivityFeed/Feed.jsx
+++ src/js/views/Dashboard/ActivityFeed/Feed.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { Fragment, useContext, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import InfiniteScroll from 'react-infinite-scroll-component'
 
@@ -86,18 +86,14 @@ function Feed({ onReady }) {
               {state.data.map((entry, index) => {
                 let entryComponent = <></>
                 if (entry.type === 'ProjectFeedEntry')
-                  entryComponent = (
-                    <ActivityEntry key={`entry-${index}`} entry={entry} />
-                  )
+                  entryComponent = <ActivityEntry entry={entry} />
                 else if (entry.type === 'OperationsLogEntry')
-                  entryComponent = (
-                    <OpsLogEntry key={`entry-${index}`} entry={entry} />
-                  )
+                  entryComponent = <OpsLogEntry entry={entry} />
                 return (
-                  <>
+                  <Fragment key={`entry-${index}`}>
                     {entryComponent}
                     <div className="h-[1px] w-full bg-gray-200"></div>
-                  </>
+                  </Fragment>
                 )
               })}
             </InfiniteScroll>
