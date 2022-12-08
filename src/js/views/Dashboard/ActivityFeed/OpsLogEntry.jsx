23be85a226baae0f2c77d50c2e353b108bb6a3dd
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2022-12-08 11:25:19 -0500

    Remove nested <a> on activity feed ops log entries
    The workaround is to remove the <a> and register an onClick handler
    for the entire <li>, which aborts if the event.target itself has an onClick
    handler (since the nested <a> for projects will).

--- src/js/views/Dashboard/ActivityFeed/OpsLogEntry.jsx
+++ src/js/views/Dashboard/ActivityFeed/OpsLogEntry.jsx
@@ -1,12 +1,13 @@
 import { DateTime } from 'luxon'
 import Gravatar from 'react-gravatar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 
 function OpsLogEntry({ entry }) {
   const { t, i18n } = useTranslation()
+  const navigate = useNavigate()
 
   const recordedAt = DateTime.fromISO(entry.recorded_at)
   const changeType = entry.change_type.toLowerCase()
@@ -43,36 +44,39 @@ function OpsLogEntry({ entry }) {
   }
 
   return (
-    <li>
-      <Link
-        to={`/ui/operations-log?v=${entry.id}`}
-        className="flex p-2 space-x-3">
-        <Gravatar
-          className="h-8 w-8 rounded-full"
-          default="mp"
-          email={entry.email_address}
-          size={22}
-        />
-        <div className="min-w-0 flex-1 text-sm text-gray-700">
-          <Trans i18nKey={i18nKey} i18n={i18n} t={t}>
-            <span className="font-medium text-gray-700">{{ displayName }}</span>
-            <span>{{ description }}</span>
-            <span>{{ changeType }}</span>
-            <Link
-              to={`/ui/projects/${entry.project_id}`}
-              className="font-medium text-blue-700 hover:text-blue-800">
-              {{ project }}
-            </Link>
-            <span className="font-medium text-gray-700">{{ environment }}</span>
-            <span className="font-medium text-gray-700">{{ version }}</span>
-          </Trans>
-          <p
-            className="mt-0.5 text-sm text-gray-500"
-            title={recordedAt.toLocaleString(DateTime.DATETIME_MED)}>
-            {recordedAt.toRelative()}
-          </p>
-        </div>
-      </Link>
+    <li
+      className="cursor-pointer flex p-2 space-x-3"
+      onClick={(event) => {
+        if (event.target.onclick === null) {
+          event.preventDefault()
+          navigate(`/ui/operations-log?v=${entry.id}`)
+        }
+      }}>
+      <Gravatar
+        className="h-8 w-8 rounded-full"
+        default="mp"
+        email={entry.email_address}
+        size={22}
+      />
+      <div className="min-w-0 flex-1 text-sm text-gray-700">
+        <Trans i18nKey={i18nKey} i18n={i18n} t={t}>
+          <span className="font-medium text-gray-700">{{ displayName }}</span>
+          <span>{{ description }}</span>
+          <span>{{ changeType }}</span>
+          <Link
+            to={`/ui/projects/${entry.project_id}`}
+            className="font-medium text-blue-700 hover:text-blue-800">
+            {{ project }}
+          </Link>
+          <span className="font-medium text-gray-700">{{ environment }}</span>
+          <span className="font-medium text-gray-700">{{ version }}</span>
+        </Trans>
+        <p
+          className="mt-0.5 text-sm text-gray-500"
+          title={recordedAt.toLocaleString(DateTime.DATETIME_MED)}>
+          {recordedAt.toRelative()}
+        </p>
+      </div>
     </li>
   )
 }
