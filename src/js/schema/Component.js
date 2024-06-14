9c0af36d8d399ecb16af376c99c6ec7561b45a5e
Dave Shawley <daves@aweber.com>
2024-06-14 13:20:22 -0400

    Add Components view in the Admin section
    This makes use of the custom JSON schema validator and the "save empty
    string as null" option. Other than that, it is a standard Admin CRUD
    view.

---
+++ src/js/schema/Component.js
@@ -0,0 +1,30 @@
+export const jsonSchema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  type: 'object',
+  properties: {
+    package_url: {
+      type: 'string',
+      pattern: 'pkg:[^:]+:.*'
+    },
+    name: { type: 'string' },
+    status: {
+      type: 'string',
+      enum: ['Active', 'Deprecated', 'Forbidden']
+    },
+    home_page: {
+      oneOf: [{ type: 'string', format: 'http-url' }, { type: 'null' }],
+      default: null
+    },
+    icon_class: { type: 'string' },
+    active_version: { oneOf: [{ type: 'string' }, { type: 'null' }] }
+  },
+  additionalProperties: true,
+  required: [
+    'active_version',
+    'home_page',
+    'icon_class',
+    'name',
+    'package_url',
+    'status'
+  ]
+}
