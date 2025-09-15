e7f417f2e2e05be8b08674b1999c410758533b9d
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2025-09-15 19:15:44 -0400

    Handle falsey values passed to ScoreBadge
    There are scenarios where (at least) undefined is passed. This ensures
    that it's always 0 and doesn't throw an error accessing toLocaleString.

--- src/js/components/ScoreBadge/ScoreBadge.jsx
+++ src/js/components/ScoreBadge/ScoreBadge.jsx
@@ -5,6 +5,7 @@ import { Badge } from '../'
 
 function ScoreBadge({ value }) {
   let color = 'red'
+  if (!value) value = 0
   if (value === 0) color = 'gray'
   if (value > 69) color = 'yellow'
   if (value > 89) color = 'green'
